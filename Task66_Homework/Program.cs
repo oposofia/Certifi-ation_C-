dac14c0bc456da46ceb6a390abb9ac19022019b6
OpoSofia <sko88@bk.ru>
2023-10-20 23:07:47 +0400

    Add files

---
+++ Task66_Homework/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов 
+// в промежутке от M до N. Выполнить с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SumRangeNaturalNumbers(int numM, int numN) // 2,5 - 3,5 - 4,5 - 5,5
+{
+    if (numM == numN) return numM;
+    if (numM < numN)
+    {
+        return numM + SumRangeNaturalNumbers(numM + 1, numN); //2 3 4
+    } //return (5)+4+3+2
+    else 
+    {
+        return numM + SumRangeNaturalNumbers(numM - 1, numN);
+    }
+    // else Console.Write($"{num1}");
+
+}
+
+Console.WriteLine("Введите натуральное число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите натуральное число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if ((n < 1) || (m < 1))
+{
+    Console.WriteLine("Некорректный ввод!");
+    return;
+}
+
+if (m<n) Console.Write($"Сумма натуральных элементов между {m} и {n} -> ");
+else Console.Write($"Сумма натуральных элементов между {n} и {m} -> ");
+Console.WriteLine(SumRangeNaturalNumbers(m, n));
+
---
+++ Task68_Homework/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int Akkerman(int numM, int numN) // 2,5 - 3,5 - 4,5 - 5,5
+{
+    if (numM == 0) return numN+1;
+    if (numN == 0)
+    {
+        return Akkerman(numM - 1, 1); //2 3 4
+    } //return (5)+4+3+2
+    else 
+    {
+        return Akkerman(numM - 1, Akkerman(numM, numN-1));
+    }
+    // else Console.Write($"{num1}");
+
+}
+
+Console.WriteLine("Введите неотрицательное число m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите неотрицательное число n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if ((n < 0) || (m < 0))
+{
+    Console.WriteLine("Некорректный ввод!");
+    return;
+}
+
+Console.WriteLine(Akkerman(m,n));
